e3a1dee27eea6403d1553c797374ebf4df24895c
seiko <seiko_des@outlook.com>
2023-12-16 15:49:29 +0800

    update gradle tasks

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalDistributionDsl
-import org.jetbrains.kotlin.gradle.targets.js.ir.JsIrBinary
 
 plugins {
     kotlin("multiplatform")
@@ -9,7 +8,8 @@ plugins {
 version = "0.0.1"
 
 val generatedLibName = "shared"
-val generatedKmpFileDir = file("$projectDir/../TestDemo/entry/src/main/ets/kmp")
+val generatedKmpFileDir get() = "$projectDir/dist"
+val ohosJsDir get() = "$projectDir/../TestDemo/entry/src/main/ets/kmp"
 
 kotlin {
     js(IR) {
@@ -20,28 +20,12 @@ kotlin {
             }
             @OptIn(ExperimentalDistributionDsl::class)
             distribution {
-                outputDirectory.set(generatedKmpFileDir)
+                outputDirectory.set(file(generatedKmpFileDir))
             }
         }
         binaries.library()
         generateTypeScriptDefinitions()
-        // ≈ useEsModules()
-        compilations.all {
-            kotlinOptions {
-                moduleKind = "es"
-                sourceMap = false
-                metaInfo = false
-            }
-            binaries.withType<JsIrBinary>().all {
-                linkTask.configure {
-                    kotlinOptions {
-                        moduleKind = "es"
-                        sourceMap = false
-                        metaInfo = false
-                    }
-                }
-            }
-        }
+        useEsModules()
     }
     sourceSets {
         all {
@@ -65,7 +49,12 @@ kotlin {
 // rename old file when rebuild
 val jsBrowserDeleteLibrary by tasks.register<Delete>("jsBrowserDeleteLibrary") {
     group = "kotlin browser"
-    delete(file(generatedKmpFileDir))
+    delete(generatedKmpFileDir)
+}
+
+val ohosDeleteLibrary by tasks.register<Delete>("ohosDeleteLibrary") {
+    group = "ohos"
+    delete(ohosJsDir)
 }
 
 listOf(
@@ -74,28 +63,80 @@ listOf(
 ).forEach { taskName ->
     tasks.getByName(taskName).dependsOn(jsBrowserDeleteLibrary)
 
-    // ohos not support *.mjs, rename shared.mjs to shared.js
-    val renameShareLib by tasks.register<Copy>("${taskName}RenameKmpLib") {
-        group = "kotlin browser"
+    tasks.register<Copy>(taskName.replace("jsBrowser", "ohosJs")) {
+        group = "ohos"
+        dependsOn(taskName)
+        dependsOn(ohosDeleteLibrary)
+
+        val targetDir = ohosJsDir
+
         from(generatedKmpFileDir)
-        into(generatedKmpFileDir)
-        include("${generatedLibName}.mjs")
-        rename("${generatedLibName}.mjs", "${generatedLibName}.js")
-        dependsOn(tasks.getByName(taskName))
+        into(targetDir)
+
+        include("*.mjs")
+        include("*.ts")
+
+        // *.mjs -> *.js
+        rename("(.*).mjs", "$1.js")
+        filter { line ->
+            line.replace(".mjs'", ".js'")
+        }
+
+        // remove follow codes in compose-multiplatform-core-runtime.mjs
+        filterContent(
+            "function MovableContent(content) {",
+            "illegalDecoyCallException('<init>');",
+            "}",
+        )
+        // remove follow codes in compose-multiplatform-core-ui.mjs
+        filterContent(
+            "function ComposedModifier(inspectorInfo, factory) {",
+            "illegalDecoyCallException('<init>');",
+            "}"
+        )
+        filterContent(
+            "function NodeState(slotId, content, composition) {",
+            "composition = composition === VOID ? null : composition;",
+            "illegalDecoyCallException('<init>');",
+            "}"
+        )
     }
+}
 
-    // TODO create task: remove this in compose-multiplatform-core-runtime.mjs
-    // function MovableContent(content) {
-    //   illegalDecoyCallException('<init>');
-    // }
-
-    // rename task will not delete old file, so run this task
-    tasks.register<Delete>("${taskName}Fix") {
-        group = "kotlin browser"
-        delete(File(generatedKmpFileDir, "${generatedLibName}.mjs"))
-        dependsOn(renameShareLib)
+// file: -> filter("b", "c") -> file:
+//   a                            a
+//   b                            d
+//   c
+//   d
+fun Copy.filterContent(vararg content: String) {
+    var checkIndex = 0
+    filter { line ->
+        if (line.contains(content[checkIndex])) {
+            checkIndex++
+            if (checkIndex == content.size) {
+                checkIndex = 0
+                return@filter null
+            }
+            return@filter null
+        }
+        if (checkIndex > 0) {
+            if (checkIndex == content.size) {
+                checkIndex = 0
+                return@filter null
+            } else {
+                return@filter buildString {
+                    content.take(checkIndex).forEach {
+                        append(it)
+                        append('\n')
+                    }
+                    checkIndex = 0
+                    append(line)
+                }
+            }
+        }
+        line
     }
 }
 
 // run follow task to build lib
-// ./gradlew :shared:jsBrowserDevelopmentLibraryDistributionFix
+// ./gradlew :shared:ohosJsDevelopmentLibraryDistribution
